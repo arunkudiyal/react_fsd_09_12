4ed12daf0a3ac92b3c2acbf45555998649d5224f
arunkudiyal <arunkudiyal@icloud.com>
2022-05-17 10:34:22 +0530

    Creating States, Handling Events & Manipulation States

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import { Component } from 'react'
-import Button from "./Button";
 import Person from './components/Person';
 
 class App extends Component {
-  render() {
-    // You can write JS
+  // You can write JS
+  // If you want to manage your own data inside the component | STATE
+  // State is nothing but a simple JS Object
+  // States are IMMUTABLE - cannot be changed directly
+  state = {
+    persons: [
+      { name: 'Benjamin', age: '23' },
+      { name: 'John', age: '25' },
+      { name: 'Max', age: '28' }
+    ],
+    anotherState: 'This is an another state'
+  }
 
+  switchNameHandler = () => {
+    // DONOT DO THIS - this.state.persons[0].name = 'Ben' --> State will become unpredictable
+    this.setState({
+      persons: [
+        { name: 'Ben', age: '23' },
+        { name: 'Johnatthan', age: '25' },
+        { name: 'Maxilliam', age: '28' }
+      ]
+    })
+  }
+
+  render() {
     return (
       <div>
         <div className="container-fluid">
@@ -13,8 +34,10 @@ class App extends Component {
         </div> <hr />
         <div className="container">
           {/* Props are a way to customise the Component */}
-          <Person name="Benjamin" age="23" />
-          <Button color="success" />
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >Dummy Text 1</Person>
+          <button onClick={this.switchNameHandler} className='btn btn-danger'>Switch Name</button>
         </div>
       </div>
     )
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,8 +1,14 @@
 function Person(props) {
+    // JavaScript
     return (
         <div className="container">
-            <p>I am {props.name} and I am {props.age} years old! </p>
+            <p className="lead">I am {props.name} and I am {props.age} years old! </p>
+            {/* {props.children} */}
         </div>
     )
+}
+Person.defaultProps = {
+    name: 'Dummy Name',
+    age: 0
 }
 export default Person;
